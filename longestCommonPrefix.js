fc3d317cc47df7b771649c24c07bcc6f36cced89
mika <2865911620@qq.com>
2023-06-16 13:39:02 +0800

    使用for...in提高时间复杂度

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -22,13 +22,18 @@
 var longestCommonPrefix = function (strs) {
     if (strs.length === 0) return "";
     let prefix = strs[0];
-    
-    for (let i = 1; i < strs.length; i++) {
+    for(let i in strs){
         while (strs[i].indexOf(prefix) !== 0) {
             prefix = prefix.slice(0, prefix.length - 1);
             if (prefix === "") return "";
         }
     }
+    // for (let i = 1; i < strs.length; i++) {
+    //     while (strs[i].indexOf(prefix) !== 0) {
+    //         prefix = prefix.slice(0, prefix.length - 1);
+    //         if (prefix === "") return "";
+    //     }
+    // }
     return prefix;
 };
 
